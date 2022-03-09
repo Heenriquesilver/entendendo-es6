a8c6a551629566953b0c9ba5660ee55d0e1a8b85
Luiz Henrique <87606092+Heenriquesilver@users.noreply.github.com>
2022-03-09 13:42:20 -0300

    Update 01_parOuImpar.js

--- Cap03 - Métodos auxiliares para Array/gabarito/01_parOuImpar.js
+++ Cap03 - Métodos auxiliares para Array/gabarito/01_parOuImpar.js
@@ -8,7 +8,7 @@ for(var i = 0; i<= numeros.length; i++) {
 }*/
 
 var numeros = [0,1,2,3,4,5];
-numeros.forEach(function(numero) {
+numeros.forEach((numero) => {
     if(numero % 2 === 0) console.log(numero + ' é par');
     else console.log(numero + ' é ímpar');
 });
